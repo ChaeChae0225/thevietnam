cf78976f0fdcbf64869cb7cdbb317328f70d489a
SohnChaeWon <good6637@naver.com>
2023-05-30 19:35:26 +0900

    Email 개수

--- src/components/Consulting.js
+++ src/components/Consulting.js
@@ -264,7 +264,8 @@ export default function Consulting() {
                   >
                     <option value=''>직접입력</option>
                     <option value='naver.com'>naver.com</option>
-                    <option value='hanmail.net'>hanmail.net</option>
+                    <option value='gmail.com'>gmail.com</option>
+                    <option value='daum.net'>daum.net</option>
                     <option value='google.com'>google.com</option>
                   </select>
                 </div>
